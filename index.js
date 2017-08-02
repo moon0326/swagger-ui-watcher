bcc6844dd43d13980e81b20ae909d6d2da75d7cc
moon0326 <moon.kyong@demandmedia.net>
2017-08-01 18:24:29 -0700

    Imrpoved error display

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 var path = require('path');
 var fs = require('fs');
 var open = require('open');
-var swaggerUiDistPath = path.dirname(require.resolve('swagger-ui-dist'));
+var nodeModules = path.resolve(path.resolve(__dirname, ''), 'node_modules');
 var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
@@ -11,28 +11,34 @@ var io = require('socket.io')(server);
 var watch = require('node-watch');
 var swaggerParser = require('swagger-parser');
 
-app.get('/', function(req, res) {
-  res.sendFile(__dirname + "/index.html");
-});
-
-app.use(express.static(swaggerUiDistPath));
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-
-io.on('connection', function(client) {  
-    console.log('Client connected.');
-});
-
-
 function start(swaggerFile, targetDir, port, hostname, bundleTo) {
+  app.get('/', function(req, res) {
+    res.sendFile(__dirname + "/index.html");
+  });
+
+  app.use(express.static(nodeModules + "/swagger-ui-dist"));
+  app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
+  io.on('connection', function(socket) {  
+    socket.on('uiReady', function(data) {
+      swaggerParser.bundle(swaggerFile, function(err, bundled) {
+        if (err) {
+          socket.emit('showError', err);
+          return;
+        }
+        socket.emit('updateSpec', JSON.stringify(bundled));
+      });       
+    });
+  });
 
   watch(targetDir, {recursive: true}, function(eventType, name) {
     swaggerParser.bundle(swaggerFile, function(err, bundled) {
       if (err) {
-        console.error(err);
+        io.sockets.emit('showError', err);
         return;
       }
       console.log("File changed. Sent updated spec to the browser.");
@@ -41,24 +47,13 @@ function start(swaggerFile, targetDir, port, hostname, bundleTo) {
       if (typeof bundleTo === 'string') {
         fs.writeFile(bundleTo, bundleString, function(err) {
           if (err) {
-            console.error(err);
+            io.sockets.emit('showError', err);
             return;
           }
-
           console.log('Saved bundle file at ' + bundleTo);
         });
       }
     });  
-  });
-
-  app.get('/spec', function(req, res) {
-    swaggerParser.bundle(swaggerFile, function(err, bundled) {
-      if (err) {
-        res.send(JSON.stringify(err));
-        return;
-      }
-      res.send(JSON.stringify(bundled));
-    });      
   });
 
   server.listen(port,hostname, function() {
