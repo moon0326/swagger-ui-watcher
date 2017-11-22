d729825ea7508397c3fe61a9a783a088d0a7f84d
yewton <yewton@gmail.com>
2017-11-22 11:03:46 +0900

    Support OAS 3.0
    * Use generic JSON refs library instead of swagger-parser

--- index.js
+++ index.js
@@ -3,14 +3,37 @@
 var path = require('path');
 var fs = require('fs');
 var open = require('open');
-var nodeModules = path.resolve(path.resolve(__dirname, ''), 'node_modules');
 var swaggerUiDist = path.dirname(require.resolve('swagger-ui-dist'));
 var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 var watch = require('node-watch');
-var swaggerParser = require('swagger-parser');
+var JsonRefs = require('json-refs');
+var yaml = require('js-yaml');
+
+function bundle(swaggerFile) {
+  var root = yaml.safeLoad(fs.readFileSync(swaggerFile, 'utf8'));
+  var options = {
+    filter : ['relative', 'remote'],
+    location: swaggerFile,
+    loaderOptions : {
+      processContent : function (res, callback) {
+        callback(undefined, yaml.safeLoad(res.text));
+      }
+    }
+  };
+  JsonRefs.clearCache();
+  return JsonRefs.resolveRefs(root, options).then(function (results) {
+    return results.resolved;
+  }, function (e) {
+      var error = {};
+      Object.getOwnPropertyNames(e).forEach(function (key) {
+        error[key] = e[key];
+      });
+      return Promise.reject(error);
+  });
+}
 
 function start(swaggerFile, targetDir, port, hostname, bundleTo) {
   app.get('/', function(req, res) {
@@ -26,22 +49,16 @@ function start(swaggerFile, targetDir, port, hostname, bundleTo) {
 
   io.on('connection', function(socket) {  
     socket.on('uiReady', function(data) {
-      swaggerParser.bundle(swaggerFile, function(err, bundled) {
-        if (err) {
-          socket.emit('showError', err);
-          return;
-        }
+      bundle(swaggerFile).then(function (bundled) {
         socket.emit('updateSpec', JSON.stringify(bundled));
-      });       
+      }, function (err) {
+        socket.emit('showError', err);
+      });
     });
   });
 
   watch(targetDir, {recursive: true}, function(eventType, name) {
-    swaggerParser.bundle(swaggerFile, function(err, bundled) {
-      if (err) {
-        io.sockets.emit('showError', err);
-        return;
-      }
+    bundle(swaggerFile).then(function (bundled) {
       console.log("File changed. Sent updated spec to the browser.");
       var bundleString = JSON.stringify(bundled, null, 2);
       io.sockets.emit('updateSpec', bundleString);
@@ -54,7 +71,9 @@ function start(swaggerFile, targetDir, port, hostname, bundleTo) {
           console.log('Saved bundle file at ' + bundleTo);
         });
       }
-    });  
+    }, function (err) {
+      io.sockets.emit('showError', err);
+    });
   });
 
   server.listen(port,hostname, function() {
